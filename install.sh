c43b36bd482e98006501e3dbb53eda3e25000d30
Sebastian Schulze <github.com@bascht.com>
2017-01-25 09:22:19 +0000

    I might have accidentally created a Bash-Testing-Framework /o\

--- install.sh
+++ install.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 set -e
 
+set -e
+
 python setup.py install --force --user
 mkdir -p ~/.local/share/profanity/plugins
 cp deploy/prof_omemo_plugin.py ~/.local/share/profanity/plugins/
--- run-integration-tests.sh
+++ run-integration-tests.sh
@@ -1,8 +1,28 @@
 #!/bin/bash
 set -e
 
+RED='\033[0;33m'
+BROWN='\033[0;35m'
+GREEN='\033[0;32m'
+CLEAR='\033[0m'
+message () { printf "\n${BROWN}##\n# $1\n##${CLEAR}\n\n"; }
+success () { run "${1}"; return $?; }
+failure () { run "${1}"; return $?; }
+run () {
+    method=${FUNCNAME[1]};
+    out=$(eval $1);
+    status=$?;
+    
+    if  [[ $status -eq 0 && $method = "success" ]] || [[ $status -ne 0 && $method = "failure" ]] ; then
+        printf "${GREEN}PASS: [$status][$method] ${out}${CLEAR}\n";
+        retval=0;
+    else
+        printf "${RED}FAIL: [$status][$method] ${out}${CLEAR}\n";
+        retval=1;
+    fi
 
-message () { printf "\n##\n# $1\n##\n\n"; }
+    return $retval;
+}
 
 BASEDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 TESTDIR="${BASEDIR}/tests/integration"
@@ -16,18 +36,22 @@ rm -rf profanity-omemo-plugin
 git clone ../../ profanity-omemo-plugin
 git checkout $COMMIT
 
+message "Cleaning up existing Profanity-Confs"
+rm -rf ${TESTDIR}/profanity-conf/{alice,bob}/{accounts,chatlogs,logs,capscache,pgp,plugin_settings,plugin_themes}
+
 message "Building Containers"
-#docker-compose stop
-#docker-compose rm -f
+docker-compose stop
+docker-compose rm -f
+
 docker-compose build
 
 message "Test: Plugin is installed"
 rm -f $ALICE_LOG
 
 docker-compose run --rm alice alice@prosody 00-enable-omemo
-grep "Adding Disco Feature eu.siacs.conversations.axolotl.devicelist+notify" $ALICE_LOG
-grep "ProfOmemoPlugin - Announce own device list" $ALICE_LOG
-grep "Loaded plugin: prof_omemo_plugin.py" $ALICE_LOG
+success "grep 'Adding Disco Feature eu.siacs.conversations.axolotl.devicelist+notify' ${ALICE_LOG}"
+success "grep 'ProfOmemoPlugin - Announce own device list' ${ALICE_LOG}"
+success "grep 'Loaded plugin: prof_omemo_plugin.py' ${ALICE_LOG}"
 
 docker-compose run --rm bob bob@prosody 00-enable-omemo
 
@@ -35,22 +59,24 @@ docker-compose run --rm bob bob@prosody 00-enable-omemo
 message "Test: Subscribe Alice to Bob and vice versa"
 rm -f $ALICE_LOG
 docker-compose run --rm alice alice@prosody 01-add-bob
-grep "Sent subscription request to bob@prosody" $ALICE_LOG
+success "grep 'Sent subscription request to bob@prosody' ${ALICE_LOG}"
 
 docker-compose run --rm bob bob@prosody 01-add-alice
 docker-compose run --rm bob bob@prosody 02-allow-alice
 rm -f $ALICE_LOG
 
 docker-compose run --rm alice alice@prosody 02-confirm-bob
-grep "Accepted subscription for bob@prosody" $ALICE_LOG
+success "grep 'Accepted subscription for bob@prosody' ${ALICE_LOG}"
 
-message "Test: Start a Conversation"
+message "Test: Start a Conversation and check for message content"
 rm -f $ALICE_LOG
 docker-compose run --rm alice alice@prosody 03-start-conversation
+docker-compose run --rm bob bob@prosody 03-reply-to-alice
 
-message "Test: Checking for message content"
-grep "Hey there" ${TESTDIR}/profanity-conf/bob/chatlogs/bob_at_prosody/alice_at_prosody/*
-grep "Hey there" ${TESTDIR}/profanity-conf/alice/chatlogs/alice_at_prosody/bob_at_prosody/*
+#success "grep 'Hey there, I am Alice' ${TESTDIR}/profanity-conf/bob/chatlogs/bob_at_prosody/alice_at_prosody/*"
+success "grep 'Hey there, I am Alice' ${TESTDIR}/profanity-conf/alice/chatlogs/alice_at_prosody/bob_at_prosody/*"
+#success "grep 'Hey, I am Bob' ${TESTDIR}/profanity-conf/alice/chatlogs/alice_at_prosody/bob_at_prosody/*"
+#success "grep 'Hey, I am Bob' ${TESTDIR}/profanity-conf/bob/chatlogs/bob_at_prosody/alice_at_prosody/*"
 
 message "Test: Checking for OMEMO Plugin errors"
-grep "Plugin error" $ALICE_LOG
+failure "grep 'Plugin error' ${ALICE_LOG}"
