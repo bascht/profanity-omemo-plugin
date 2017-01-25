1d34278bda6e17ba823f07baf27a28f1812fd0d8
Sebastian Schulze <github.com@bascht.com>
2017-01-25 13:25:10 +0000

    Fix test invocation without ENTRYPOINT

--- run-integration-tests.sh
+++ run-integration-tests.sh
@@ -56,30 +56,30 @@ success "docker-compose run --rm alice profanity --version | grep 'Profanity, ve
 message "Test: Plugin is installed"
 rm -f $ALICE_LOG
 
-docker-compose run --rm alice alice@prosody 00-enable-omemo
+docker-compose run --rm alice profanity-script.sh alice@prosody 00-enable-omemo
 success "grep 'Adding Disco Feature eu.siacs.conversations.axolotl.devicelist+notify' ${ALICE_LOG}"
 success "grep 'ProfOmemoPlugin - Announce own device list' ${ALICE_LOG}"
 success "grep 'Loaded plugin: prof_omemo_plugin.py' ${ALICE_LOG}"
 
-docker-compose run --rm bob bob@prosody 00-enable-omemo
+docker-compose run --rm bob profanity-script.sh bob@prosody 00-enable-omemo
 
 
 message "Test: Subscribe Alice to Bob and vice versa"
 rm -f $ALICE_LOG
-docker-compose run --rm alice alice@prosody 01-add-bob
+docker-compose run --rm alice profanity-script.sh alice@prosody 01-add-bob
 success "grep 'Sent subscription request to bob@prosody' ${ALICE_LOG}"
 
-docker-compose run --rm bob bob@prosody 01-add-alice
-docker-compose run --rm bob bob@prosody 02-allow-alice
+docker-compose run --rm bob profanity-script.sh bob@prosody 01-add-alice
+docker-compose run --rm bob profanity-script.sh bob@prosody 02-allow-alice
 rm -f $ALICE_LOG
 
-docker-compose run --rm alice alice@prosody 02-confirm-bob
+docker-compose run --rm alice profanity-script.sh alice@prosody 02-confirm-bob
 success "grep 'Accepted subscription for bob@prosody' ${ALICE_LOG}"
 
 message "Test: Start a Conversation and check for message content"
 rm -f $ALICE_LOG
-docker-compose run --rm alice alice@prosody 03-start-conversation
-docker-compose run --rm bob bob@prosody 03-reply-to-alice
+docker-compose run --rm alice profanity-script.sh alice@prosody 03-start-conversation
+docker-compose run --rm bob profanity-script.sh bob@prosody 03-reply-to-alice
 
 #success "grep 'Hey there, I am Alice' ${TESTDIR}/profanity-conf/bob/chatlogs/bob_at_prosody/alice_at_prosody/*"
 success "grep 'Hey there, I am Alice' ${TESTDIR}/profanity-conf/alice/chatlogs/alice_at_prosody/bob_at_prosody/*"
